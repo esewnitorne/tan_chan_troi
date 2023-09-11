ebaa7edd0b03ecd09bf879634cdec0cd0e19721d
esewnitorne <144709046+esewnitorne@users.noreply.github.com>
2023-09-11 15:38:08 +0000

    cn

---
+++ m-r-b-l-u-e.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         m-r-b-l-u-e
+// @namespace    https://github.com/esewnitorne/tan_chan_troi/raw/main/m-r-b-l-u-e.user.js
+// @version      2023.09.11
+// @description  down m-r-b-l-u-e
+// @author       You
+// @match        https://viewer.mrblue.com/comics/*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mrblue.com
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @grant        none
+// @downloadURL  https://github.com/esewnitorne/tan_chan_troi/raw/main/m-r-b-l-u-e.user.js
+// @updateURL    https://github.com/esewnitorne/tan_chan_troi/raw/main/m-r-b-l-u-e.meta.js
+// ==/UserScript==
